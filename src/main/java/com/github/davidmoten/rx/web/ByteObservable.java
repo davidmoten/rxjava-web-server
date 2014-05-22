3bbe53636cbd438c030f010ed04e7001786c9355
Dave Moten <davidmoten@gmail.com>
2014-05-23 06:26:35 +1000

    add byteobs

--- src/main/java/com/github/davidmoten/rx/web/ByteObservable.java
+++ src/main/java/com/github/davidmoten/rx/web/ByteObservable.java
@@ -3,7 +3,9 @@ package com.github.davidmoten.rx.web;
 import java.io.ByteArrayOutputStream;
 
 import rx.Observable.Operator;
+import rx.Observer;
 import rx.Subscriber;
+import rx.observers.Subscribers;
 
 public class ByteObservable {
 
@@ -14,8 +16,34 @@ public class ByteObservable {
 
 			@Override
 			public Subscriber<? super byte[]> call(
-					Subscriber<? super byte[]> subscriber) {
+					final Subscriber<? super byte[]> subscriber) {
+				Subscriber<byte[]> sub = Subscribers
+						.from(new Observer<byte[]>() {
 
+							@Override
+							public void onCompleted() {
+								subscriber.onCompleted();
+							}
+
+							@Override
+							public void onError(Throwable e) {
+								subscriber.onError(e);
+							}
+
+							@Override
+							public void onNext(byte[] b) {
+								int num = Math.min(b.length, n - buffer.size());
+								if (num > 0)
+									buffer.write(b, 0, num);
+								if (buffer.size() == n) {
+									subscriber.onNext(buffer.toByteArray());
+									buffer.reset();
+									subscriber.onCompleted();
+								}
+							}
+						});
+				subscriber.add(sub);
+				return sub;
 			}
 		};
 
